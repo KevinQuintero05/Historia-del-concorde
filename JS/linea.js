c6ea225e5437c7bbc9bd186a03a4a49caeaf6c92
kevinQuintero05 <kevin.quintero06@misena.edu.co>
2023-10-16 17:28:01 -0500

    Primera carga

---
+++ JS/linea.js
@@ -0,0 +1,20 @@
+var canvas = document.getElementById("lineaT");
+var ctx = canvas.getContext("2d");
+// ctx.fillStyle = "black";
+ctx.fillRect(75, 0, 150, 900);
+
+
+var canvas = document.getElementById("lineaT1");
+var ctx = canvas.getContext("2d");
+// ctx.fillStyle = "black";
+ctx.fillRect(75, 0, 150, 900);
+
+// ctx.font = '24px serif';
+// ctx.fillStyle = "white";
+// ctx.textAlign = 'left';
+// ctx.fillText("1960´s",120,90);
+
+var canvas = document.getElementById("lineaP");
+var ctx = canvas.getContext("2d");
+// ctx.fillStyle = "black";
+ctx.fillRect(75, 0, 150, 1000);
